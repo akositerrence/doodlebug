0a8ee9c20a88b7a28d83aa24ee56b49bb9d6b42c
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2025-02-07 11:46:38 -0600

    relay test

--- control.py
+++ control.py
@@ -5,22 +5,34 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 spi_a = spidev.SpiDev()
-
-spi_a.max_speed_hz = 500000
-
 thermocouple_so_a = 21      # GPIO 9 ( SPI0 MISO )
 thermocouple_cs_a = 24      # GPIO 8 ( SPI0 CE0 )
 thermocouple_sck_a = 23     # GPIO 11 ( SPI0 SCLK )
+
+ent_cooling_valve = 1       # GPIO 1
+ext_cooling_valve = 1       # GPIO 1
+ent_gas_valve = 1           # GPIO 1
+ext_gas_valve = 1           # GPIO 1
 
 def setup():
     GPIO.setup(thermocouple_so_a, GPIO.OUT)
     GPIO.setup(thermocouple_cs_a, GPIO.OUT)
     GPIO.setup(thermocouple_sck_a, GPIO.OUT)
+    GPIO.setup(ent_cooling_valve, GPIO.OUT)
+    GPIO.setup(ext_cooling_valve, GPIO.OUT)
+    GPIO.setup(ent_gas_valve, GPIO.OUT)
+    GPIO.setup(ext_gas_valve, GPIO.OUT)
 
-def read_sensors():
-    temp_a = GPIO.input(thermocouple_so_a)
-    sensor_data = [temp_a]
-    return(sensor_data)
+def test_relay():
+    while True:
+        GPIO.output(ent_cooling_valve, GPIO.LOW)  
+        GPIO.output(ext_cooling_valve, GPIO.LOW)   
+        GPIO.output(ent_gas_valve, GPIO.LOW)   
+        GPIO.output(ext_gas_valve, GPIO.LOW)
+        time.sleep(2)
+        GPIO.output(ent_cooling_valve, GPIO.HIGH)  
+        GPIO.output(ext_cooling_valve, GPIO.HIGH)   
+        GPIO.output(ent_gas_valve, GPIO.HIGH)   
+        GPIO.output(ext_gas_valve, GPIO.HIGH)
 
-read_sensors()
-
+test_relay()
