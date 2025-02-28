d80c66fe23c1fa52732dcfae64cac74faa83bc0c
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2025-02-28 15:00:29 -0600

    tewst

--- tests/thermocouple_test.py
+++ tests/thermocouple_test.py
@@ -8,23 +8,17 @@ spi_a = spidev.SpiDev()
 spi_a.open(0,0)
 spi_a.max_speed_hz = 500000
 
-def read_test():
+def read_test(): # account for loss of signal?
     raw = spi_a.xfer2([0x00, 0x00])
     value = (raw[0] << 8) | raw[1]
-
-    if value & 0x4:
-        return None
-    
-    return raw
+    temperature = (value >> 3) * 0.25
+    return temperature
 
 i = 0
-while i < 10:
-    raw_data = read_test()
-    if raw_data is not None:
-        print(raw_data)
-    else:
-        print('not connected')
-    time.sleep(0.1)
+while i < 100:
+    temp = read_test()
+    print(temp)
+    time.sleep(0.5)
     i = i +1
 
 spi_a.close()
