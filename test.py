1ad6c8de9ad4273d62b764258cdc667c11412a6d
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2025-09-21 20:25:41 -0500

    thermocouple debugging

--- test.py
+++ test.py
@@ -1,18 +1,32 @@
-import spidev
-import time
+import RPi.GPIO as GPIO, time
 
-spi_bus = spidev.SpiDev()
-spi_bus.open(10,0)
-spi_bus.max_speed_hz = 500000
-spi_bus.mode = 0
+GPIO.setmode(GPIO.BOARD)
 
-def read_thermocouples():
-    raw = spi_bus.readbytes(2)
-    return raw
+SO  = 21
+SCK = 23
+CS  = 29
 
-for i in range(10):
-    tc1 = read_thermocouples()
-    print(tc1)
-    time.sleep(0.01)
+GPIO.setup(SCK, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(CS, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(SO, GPIO.IN)
 
-spi_bus.close()
+def read_max6675():
+    GPIO.output(CS, GPIO.LOW)
+    time.sleep(0.001)
+
+    value = 0
+    for i in range(16):
+        GPIO.output(SCK, GPIO.HIGH)
+        time.sleep(0.0001)
+        value = (value << 1) + GPIO.input(SO)
+        GPIO.output(SCK, GPIO.LOW)
+        time.sleep(0.0001)
+
+    GPIO.output(CS, GPIO.HIGH)
+    return value
+
+raw = read_max6675()
+temp_c = (raw >> 3) * 0.25
+print(f"Raw=0x{raw:04X}, Temp={temp_c:.2f} °C")
+
+GPIO.cleanup()
