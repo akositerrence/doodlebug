44471131d7b6d645bc1c3a3ffd0274e51ffd29fe
Terrence Andre San Gabriel <terrsangabriel@gmail.com>
2025-09-23 09:52:31 -0500

    testthis

---
+++ test.py
@@ -0,0 +1,9 @@
+from max31855 import MAX31855, MAX31855Error
+
+cs_pin=24
+clock_pin=23
+data_pin=22
+unit="f"
+thermocouple1=MAX31855(cs_pin, clock_pin, data_pin)
+print(thermocouple1.get())
+thermocouple1.cleanup()
